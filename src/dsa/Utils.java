18c7a8ec7aca66ff9c8dc376500ab9ff645527b4
arunantony <anton.arun@gmail.com>
2023-08-09 15:47:46 -0500

    Initial commit

---
+++ src/dsa/Utils.java
@@ -0,0 +1,5 @@
+package src.com.dsa;
+
+public class Utils {
+    
+}
---
+++ src/dsa/steps/Steps.java
@@ -0,0 +1,56 @@
+class Steps{
+
+    void printPattern(int n, char ch){
+
+        for(int i = 0; i<n; i++){
+            for (int j = 0; j<n; j++){
+                if(j<=i){
+                    System.out.print(ch);
+                }else{
+                    System.out.print(' ');
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+
+    void printPatternUpsideDown(int n, char ch){
+
+        for(int i = 0; i<n; i++){
+            for (int j = n; j>0; j--){
+                if(j>i){
+                    System.out.print(ch);
+                }else{
+                    System.out.print(' ');
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    void printPyramidwise(int n, char ch){
+        for(int i = 0; i<n; i++){
+            for(int j = 0;j<=n+i; j++){
+                if(j>=n-i){
+                    System.out.print(ch);
+                    j++;
+                }
+                System.out.print(' ');
+            }
+
+            System.out.println("");
+        }
+    }
+
+
+}
+
+class Test{
+
+    public static void main(String[] args) {
+        Steps steps = new Steps();
+
+        steps.printPyramidwise(4, '*');
+    }
+}
